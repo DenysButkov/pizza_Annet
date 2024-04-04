0cd243283349c06603662a12648cd81f3a173d55
Denys <denysbutkov@gmail.com>
2024-04-04 16:47:02 +0200

    second commit

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import App from './App';
-import store from './redux/store'; // Змінено шлях до імпорту
+import store from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
